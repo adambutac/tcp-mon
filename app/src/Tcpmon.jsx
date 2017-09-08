3448cb15e6b8ed4e59ad606dab27229b9fc08d15
Adam <starhelp@hawaii.edu>
2017-09-08 11:10:49 -1000

    added filter

--- app/src/Tcpmon.jsx
+++ app/src/Tcpmon.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import _ from 'underscore';
-import { Button, Menu, Search, Segment, Table } from 'semantic-ui-react';
+import { Button, Dimmer, Input, Loader, Menu, Table } from 'semantic-ui-react';
 
 import Netstat from 'node-netstat';
 
 class Tcpmon extends React.Component {
   constructor(props) {
     super(props);
+
+    this.tableHeaders = ['PID', 'Protocol', 'Local Address', 'Local Port', 'Remote Address', 'Remote Port', 'TCP State'];
     this.state = {
       stats: [],
-      portOptions: [],
       stderr: '',
-      serverPort: '',
-      serverPortConnectionCount: 0,
       isRunning: false,
     }
   }
 
   run() {
     let stats = [];
-    let portOptions = [];
     this.setState({ isRunning: true });
 
     Netstat({
@@ -28,57 +25,47 @@ class Tcpmon extends React.Component {
       done: () => {
         this.setState({ stats });        
         this.setState({ isRunning: false });
-        this.setState({ portOptions })
       }
     }, (data) => {
-      /* map by local port */
-      const { port } = data.local;
-      if(!stats[port]){
-        portOptions.push({
-          key:port,
-          value: port,
-          text: port });
-        stats[port] = data;
-        stats[port].count = 0;
-      }
-      stats[port].count++;
+      stats.push(data);
     });
   }
 
-  stop() {
-    this.setState({ isRunning: false });
-  }
-
-  filter(col, regex) {
-    stats.filter(row => {
+  filter(stats, col, str) {
+    const regex = new RegExp(str);
+    return stats.filter(row => {
       if(!row[col])  
         return false;
       else 
-        return /regex/i.test(row[col]);
-    })
+        return regex.test(row[col]);
+    });
   }
 
   render() {
-    const { isLoading, stats, stderr, isRunning, serverPort, serverPortConnectionCount, portOptions } = this.state;
+    const { col, isRunning, regex, stats, stderr, serverPort } = this.state;
 
     return <div>
-      <Button content="run" loading={isRunning} onClick={() => this.run()} />
-      <strong> {serverPortConnectionCount} </strong> Established Connections on port 
+      <Menu secondary>
+        <Menu.Item name="run" active={isRunning} onClick={() => this.run()} />
+        <Menu.Item name="column">
+          <Input onChange={e => this.setState({ col: e.target.value })} />
+        </Menu.Item>
+        <Menu.Item>
+          <Input name="regex" onChange={e => this.setState({ regex: e.target.value })} />
+        </Menu.Item>
+      </Menu>
 
+      <Dimmer inverted active={isRunning}>
+        <Loader content="Loading..." />
+      </Dimmer>
       <Table striped>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell>PID</Table.HeaderCell>            
-            <Table.HeaderCell>Protocol</Table.HeaderCell>
-            <Table.HeaderCell>Local</Table.HeaderCell>
-            <Table.HeaderCell>Local Port</Table.HeaderCell>
-            <Table.HeaderCell>Remote</Table.HeaderCell>
-            <Table.HeaderCell>Remote Port</Table.HeaderCell>            
-            <Table.HeaderCell>State</Table.HeaderCell>
+            { this.tableHeaders.map((hdr, index) => <Table.HeaderCell key={index} content={hdr} />)}
           </Table.Row>
         </Table.Header>
         <Table.Body>
-        { stats.map((row, index) => {
+        { this.filter(stats, col, regex).map((row, index) => {
           return <Table.Row key={index}>
             <Table.Cell>{row.pid}</Table.Cell>
             <Table.Cell>{row.protocol}</Table.Cell>            
@@ -86,7 +73,7 @@ class Tcpmon extends React.Component {
             <Table.Cell>{row.local.port}</Table.Cell>            
             <Table.Cell>{row.remote.address}</Table.Cell>            
             <Table.Cell>{row.remote.port}</Table.Cell>            
-            <Table.Cell>{row.state}</Table.Cell>            
+            <Table.Cell>{row.state}</Table.Cell>
           </Table.Row>            
         })}
         </Table.Body>
