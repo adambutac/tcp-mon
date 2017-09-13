a2cbdd396522afb2115f05ed8bd89a4ca7283093
Adam <starhelp@hawaii.edu>
2017-09-13 10:58:07 -1000

    updated filtering and ui

--- app/src/Tcpmon.jsx
+++ app/src/Tcpmon.jsx
@@ -11,8 +11,26 @@ class Tcpmon extends React.Component {
     this.state = {
       stats: [],
       stderr: '',
+      connectionCount: 0,
+      largestConnectionCount: 0,
+      backlog: 0,
+      largestBacklog: 0,
+      maxTomcatConnections: 1,
       isRunning: false,
     }
+  }
+
+  reset() {
+    this.setState({
+      stats: [],
+      stderr: '',
+      connectionCount: 0,
+      largestConnectionCount: 0,
+      backlog: 0,
+      largestBacklog: 0,
+      maxTomcatConnections: 1,
+      isRunning: false,
+    });
   }
 
   run() {
@@ -20,44 +38,48 @@ class Tcpmon extends React.Component {
     this.setState({ isRunning: true });
 
     Netstat({
-      filter: null,
+      filter: {
+        local: {
+          port: 10011,
+        },
+        state: "ESTABLISHED"
+      },
       watch: false,
       done: () => {
-        this.setState({ stats });        
-        this.setState({ isRunning: false });
+        const connectionCount = stats.length;
+        const backlog = connectionCount > this.state.maxTomcatConnections ? connectionCount - this.state.maxTomcatConnections : 0;
+        const largestBacklog = backlog > this.state.largestBacklog ? backlog : this.state.largestBacklog;
+        this.setState({ stats });
+        this.setState({ connectionCount });
+        this.setState({ backlog });
+        this.setState({ largestBacklog });
+        if(this.state.isRunning) {
+          this.run();
+        } 
       }
     }, (data) => {
       stats.push(data);
     });
   }
 
-  filter(stats, col, str) {
-    const regex = new RegExp(str);
-    return stats.filter(row => {
-      if(!row[col])  
-        return false;
-      else 
-        return regex.test(row[col]);
-    });
+  stop() {
+    this.setState({ isRunning: false });
   }
 
   render() {
-    const { col, isRunning, regex, stats, stderr, serverPort } = this.state;
-
+    const { backlog, connectionCount, largestBacklog, isRunning, maxTomcatConnections, stats, stderr } = this.state;
+  
     return <div>
-      <Menu secondary>
-        <Menu.Item name="run" active={isRunning} onClick={() => this.run()} />
-        <Menu.Item name="column">
-          <Input onChange={e => this.setState({ col: e.target.value })} />
-        </Menu.Item>
-        <Menu.Item>
-          <Input name="regex" onChange={e => this.setState({ regex: e.target.value })} />
-        </Menu.Item>
+      <Menu secondary compact>
+        <Menu.Item name="run" disabled={isRunning} onClick={() => this.run()} />
+        <Menu.Item name="stop" disabled={!isRunning} onClick={() => this.stop()} />
+        <Menu.Item name="reset" disabled={isRunning} onClick={() => this.reset()} />
+        <Menu.Item >Estab.Connections: {connectionCount} </Menu.Item>
+        <Menu.Item >Max Connections: <Input disabled={isRunning} style={{width: "50px"}} value={maxTomcatConnections} onChange={ e => this.setState({ maxTomcatConnections: e.target.value }) }/> </Menu.Item>
+        <Menu.Item >Backlog Count: { backlog } </Menu.Item>
+        <Menu.Item >Largest Backlog Count: { largestBacklog } </Menu.Item>
       </Menu>
 
-      <Dimmer inverted active={isRunning}>
-        <Loader content="Loading..." />
-      </Dimmer>
       <Table striped>
         <Table.Header>
           <Table.Row>
@@ -65,7 +87,7 @@ class Tcpmon extends React.Component {
           </Table.Row>
         </Table.Header>
         <Table.Body>
-        { this.filter(stats, col, regex).map((row, index) => {
+        { stats.map((row, index) => {
           return <Table.Row key={index}>
             <Table.Cell>{row.pid}</Table.Cell>
             <Table.Cell>{row.protocol}</Table.Cell>            
