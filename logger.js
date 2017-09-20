c51f5d3de53f340f0e00cc345aaf874a6d046514
Adam <starhelp@hawaii.edu>
2017-09-20 11:47:21 -1000

    fixed check for logs dir

--- logger.js
+++ logger.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const EOL = require('os').EOL;
 
 function main(args) {
-  if(!fs.statSync('./logs').isDirectory()) {
+  if(!fs.existsSync('./logs')) {
     fs.mkdirSync('./logs');
   }
 
