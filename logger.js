c1ee8fc7bd5a528ed4bccdab11741bcf95da3af4
Adam <starhelp@hawaii.edu>
2017-09-20 11:28:54 -1000

    finished logger

--- logger.js
+++ logger.js
@@ -2,20 +2,28 @@ const Netstat = require('node-netstat');
 const fs = require('fs');
 
 function main(args) {
+  if(!fs.statSync('./logs').isDirectory()) {
+    fs.mkdirSync('./logs');
+  }
+
   if(args[0] == '-h' || !args[0] || !args[1]) {
-    console.log('Usage: node logger <port> <max connections> [interval]');
+    console.log('Usage: node logger <local port> <max connections> [interval]');
   } else {
-    const interval = args[2];
+    let interval = args[2];
     if(!interval) interval = 10000;
+    const localPort = args[0];
+    const maxTomcatConnections = args[1];
+
     setInterval(() => {
       let connectionCount = 0;
       
       Netstat({
         done: () => {
-          const backlog = connectionCount > maxTomcatConnections ? connectionCount - maxTomcatConnections : 0;
+          const backlogCount = connectionCount > maxTomcatConnections ? connectionCount - maxTomcatConnections : 0;
           const date = new Date();
           const currentLog = `./logs/${date.toISOString().split('T')[0]}.log`;
           fs.appendFileSync(currentLog, `${date.toISOString()}, ${connectionCount}, ${backlogCount}`);
+          console.log(`${currentLog} => ${date.toISOString()}, ${connectionCount}, ${backlogCount}`);
         }
       }, (data) => {
         if (data.local.port == localPort && (data.state == "ESTABLISHED" || data.state == "CLOSE_WAIT")){
