f27bc9cbda8d3fd23dbd1908fbd9982841d157a8
Adam <starhelp@hawaii.edu>
2017-09-25 14:28:06 -1000

    added rolling log storing most recent activity

--- app/src/logger.js
+++ app/src/logger.js
@@ -1,21 +1,23 @@
 const Netstat = require('node-netstat');
 const Timestamp = require('time-stamp');
+const Path = require('path');
 const fs = require('fs');
 const EOL = require('os').EOL;
 
 function main(args) {
-  const dir = 'C:/Development/STAR/webapps/ROOT/src/main/webapp/logs/';
+  const dir = args[0];
   if(!fs.existsSync(dir)) {
     return;
   }
 
-  if(args[0] == '-h' || !args[0] || !args[1]) {
-    console.log('Usage: node logger <local port> <max connections> [interval]');
+  if(args[0] == '-h' || !args[0] || !args[1] || !args[2]) {
+    console.log('Usage: node logger <output file directory> <local port> <max connections> [interval]');
   } else {
-    let interval = args[2];
-    if(!interval) interval = 10000;
-    const localPort = args[0];
-    const maxTomcatConnections = args[1];
+    let interval = args[3];
+    if(!interval) interval = 20000;
+    const localPort = args[1];
+    const maxTomcatConnections = args[2];
+    let latest = new Array(120000/interval);
 
     setInterval(() => {
       let connectionCount = 0;
@@ -23,9 +25,15 @@ function main(args) {
       Netstat({
         done: () => {
           const backlogCount = connectionCount > maxTomcatConnections ? connectionCount - maxTomcatConnections : 0;
-          const currentLog = `${dir}${Timestamp('YYYY-MM-DD')}.log`;
-          fs.appendFileSync(currentLog, `${Timestamp('YYYY-MM-DDTHH:mm:ss-10:00')}, ${connectionCount}, ${backlogCount}${EOL}`);
-          console.log(`${currentLog} => ${Timestamp('YYYY-MM-DDTHH:mm:ss-10:00')}, ${connectionCount}, ${backlogCount}`);
+          const currentLog = Path.join(dir,`${Timestamp('YYYY-MM-DD')}.log`);
+          const line = `${Timestamp('YYYY-MM-DDTHH:mm:ss-10:00')}, ${connectionCount}, ${backlogCount}${EOL}`;
+          latest.shift();          
+          latest.push(line);
+          
+          fs.appendFileSync(currentLog, line);
+          fs.writeFileSync(Path.join(dir, 'current.log'), latest.join(''));      
+          
+          console.log(`${currentLog} => ${line}`);
         }
       }, (data) => {
         if (data.local.port == localPort && (data.state == "ESTABLISHED" || data.state == "CLOSE_WAIT")){
